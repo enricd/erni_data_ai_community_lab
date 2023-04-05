d7eac6d5ee8ae4837c1d44908f1eabb6d3dfe3e1
enricd <edomingodomenech@gmail.com>
2023-04-05 17:08:58 +0200

    completing part of the mnist project, starting tweets sentiment analysis project

--- pages/1_🚢_Titanic_Demo.py
+++ pages/1_🚢_Titanic_Demo.py
@@ -35,7 +35,7 @@ def main():
         initial_sidebar_state="expanded",
         menu_items={
             "Report a bug": "https://github.com/enricd",
-            "About": "This is a demo Streamlit app made by Enric Domingo for the ERNI's Data and AI Community."
+            "About": "This is a Streamlit app made by the ERNI's Data & AI Community."
         }
     )
 
---
+++ pages/3_🐦_Tweets_Sentiment_Analysis.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+def main():
+
+    st.set_page_config(
+        page_title="ERNI Data & AI Community Lab",
+        page_icon="🐦",
+        layout="wide",
+        initial_sidebar_state="expanded",
+        menu_items={
+            "Report a bug": "https://github.com/enricd",
+            "About": "This is a Streamlit app made by the ERNI's Data & AI Community."
+        }
+    )
+
+    st.header("🐦 Tweets Sentiment Analysis")
+
+
+
+if __name__ == "__main__":
+
+    main()
