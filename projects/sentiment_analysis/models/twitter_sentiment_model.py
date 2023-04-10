6495aef0d88d06f71f14ad5143a2730cbeee7dba
C0DA8 <daco.wolf@gmail.com>
2023-04-10 15:39:30 +0200

    Created a class to manage the model, utils and settings files

---
+++ projects/sentiment_analysis/models/twitter_sentiment_model.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from utils.utils import *
+
+class TwitterSentimentAnalyzer():
+    def __init__(self, number_of_tweets, hashtag):
+        self.number_of_tweets = number_of_tweets
+        self.hashtag = hashtag
+        self.data = pd.DataFrame()
+        self.data_positive = pd.DataFrame()
+        self.data_neutral  = pd.DataFrame()
+        self.data_negative = pd.DataFrame()
+        self.most_common_words = ""
+        self.n_gram = []
+        self.run_model()
+
+    def data_ingestion(self):
+        self.data = tweet_importer(self.hashtag, self.number_of_tweets)
+
+    def data_cleaning(self):
+        self.data[['plain_text']] = self.data.text.apply(tweet_cleaner)
+        self.data = self.data.replace(r'^\s*$', np.nan, regex=True)
+        if self.data.isnull().sum().plain_text != 0:
+            self.data.dropna(inplace=True)
+        
+        self.data['text_lang']=self.plain_text.apply(detect_language)
+        self.data = self.data[self.data.text_lang == 'en']
+    
+    def sentiment_analysis(self):
+        for index, row in self.data['plain_text'].items():
+            score = SentimentIntensityAnalyzer().polarity_scores(row)
+            neg = score['Negative']
+            neu = score['Neutral']
+            pos = score['Positive']
+            comp = score['Compound']
+            if neg > pos:
+                self.data.loc[index, 'sentiment'] = "Negative"
+            elif pos > neg:
+                self.data.loc[index, 'sentiment'] = "Positive"
+            else:
+                self.data.loc[index, 'sentiment'] = "Neutral"
+            self.data.loc[index, 'Negative'] = neg
+            self.data.loc[index, 'Neutral'] = neu
+            self.data.loc[index, 'Positive'] = pos
+            self.data.loc[index, 'Compound'] = comp
+
+        self.data_negative = self.data[self.data['sentiment']=='Negative']
+        self.data_positive = self.data[self.data['sentiment']=='Positive']
+        self.data_neutral  = self.data[self.data['sentiment']=='Neutral']
+
+    def get_most_common_words(self):
+        countVectorizer = CountVectorizer(stop_words='english') 
+        countVector = countVectorizer.fit_transform(self.data['plain_text'])
+        negative_count_vect_df = pd.DataFrame(countVector.toarray(), columns=countVectorizer.get_feature_names_out())
+
+        count = pd.DataFrame(negative_count_vect_df.sum())
+        self.most_common_words = count.sort_values(0,ascending=False).head(20)
+    
+    def get_top_n_gram(self, ngram_range,n=None):
+        vec = CountVectorizer(ngram_range=ngram_range,stop_words = 'english').fit(self.data['plain_text'])
+        bag_of_words = vec.transform(self.data['plain_text'])
+        sum_words = bag_of_words.sum(axis=0) 
+        words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+        words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+        self.n_gram = words_freq[:n]
+
+    def run_model(self):
+        self.data_ingestion()
+        self.data_cleaning()
+        self.sentiment_analysis()
+        self.get_most_common_words()
+        self.get_top_n_gram()
---
+++ projects/sentiment_analysis/utils/utils.py
@@ -0,0 +1,52 @@
+import tweepy
+import pandas as pd
+from langdetect import detect
+from wordcloud import WordCloud, STOPWORDS
+
+from settings import *
+
+
+def tweet_importer(hashtag, number_of_tweets):
+    # Authentication
+    auth = tweepy.OAuthHandler(TWITTER_NLP_CONSUMER_KEY, TWITTER_NLP_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_NLP_ACCESS_KEY, TWITTER_NLP_ACCESS_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    # Query
+    found_tweets = [status._json['text'] for status in tweepy.Cursor(api.search_tweets, lang='en', q=hashtag).items(number_of_tweets)]
+    found_tweets.drop_duplicates(inplace = True)
+    
+    # DataFrame
+    df = pd.DataFrame(found_tweets)
+    df.rename({0:'text'},axis='columns',inplace=True)
+    return df
+
+def tweet_cleaner(raw_text):
+    text=re.sub('@[^\s]+',"", raw_text)        # Remove mentions
+    text=re.sub('#[^\s]+',"",text)             # Remove hashtags
+    text=re.sub(r"http\S+","", text)           # Remove media links
+    text=re.sub('RT ', '', text)               # remove the RT
+    text = re.sub(r'[^a-zA-Z0-9\s]+', '', text)
+    text=emoji_pattern.sub(r'',text)
+    text = text.lower()
+    text = text.replace('\n', '')
+    return  pd.Series([text])
+
+
+def detect_language(text):
+    try:
+        src_lang = detect(text)
+        return src_lang          
+    except:
+        return "NA"
+    
+def create_wordcloud(text, name):
+    stopwords = set(STOPWORDS)
+    wc = WordCloud(background_color='white', max_words=3000, stopwords=stopwords, repeat=True)
+    wc.generate(str(text))
+    wc.to_file(f'sentiment_analysis/images/{name}.png')
+
+def count_values_in_column(data,feature):
+    total=data.loc[:,feature].value_counts(dropna=False)
+    percentage=round(data.loc[:,feature].value_counts(dropna=False,normalize=True)*100,2)
+    return pd.concat([total,percentage],axis=1,keys=['Total','Percentage'])
