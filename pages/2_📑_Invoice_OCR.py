d7f8863230473f0f0b7242e20bd2589ca7750af0
Alberto <amartingomez83@gmail.com>
2023-05-15 12:25:20 +0200

    Solved camera input mode

--- pages/2_📑_Invoice_OCR.py
+++ pages/2_📑_Invoice_OCR.py
@@ -76,17 +76,8 @@ def main():
         
         #if st.button('Take a picture from your camera'):
         with st.expander('📸 Take a picture from your camera'):
-            img_file_buffer = st.camera_input("Take a picture")
-            if img_file_buffer is not None:
-                # To read image file buffer as a PIL Image:
-                img = Image.open(img_file_buffer)
+            image_raw = st.camera_input("Take a picture")
 
-                # To convert PIL Image to numpy array:
-                image_raw = np.array(img)
-
-                # Check the shape of img_array:
-                # Should output shape: (height, width, channels)
-                st.write(image_raw.shape)
         st.write ('or')
         with st.expander('📁 Choose an image file'):
             uploaded_file = st.file_uploader("Choose a image file", accept_multiple_files=False)
